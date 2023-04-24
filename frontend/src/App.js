b15d27c8ab425ec878170057412cf90db948e3d5
Shaike Mukul <mukul@Shaikes-MacBook-Pro.local>
2023-04-24 11:13:22 -0700

    logo added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>LionJet</h1>
+        <img src={logo} width={215} height={100} />
         <form className="login-form">
           <div className="form-group">
             <label htmlFor="userID">User ID:</label>
