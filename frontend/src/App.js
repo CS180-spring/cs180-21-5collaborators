5fc73f1b353ed2a3c957c2eb4c18f3c048b47e56
Shaike Mukul <mukul@Shaikes-MacBook-Pro.local>
2023-04-23 23:51:48 -0700

    loginpage added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>LionJet</h1>
-        <p>
-          Our site is currently under maintenance.
-        </p>
-        <p>
-          We apologize for any inconvenience and will be back soon!
-        </p>
+        <form className="login-form">
+          <div className="form-group">
+            <label htmlFor="userID">User ID:</label>
+            <input type="text" id="userID" name="userID" placeholder="Enter your User ID" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" required />
+          </div>
+          <button type="submit" className="login-button">Log In</button>
+        </form>
       </header>
     </div>
   );
